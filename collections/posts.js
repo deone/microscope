aa7c18345feaf00a1c4889b1074af16f7a622402
Oladayo Osikoya <alwaysdeone@gmail.com>
2013-12-15 18:22:42 +0100

    Denormalized the number of comments in the post.

--- collections/posts.js
+++ collections/posts.js
@@ -31,22 +31,14 @@ Meteor.methods({
 
 	// pick out the whitelisted keys
 	var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
-	    title: postAttributes.title + (this.isSimulation ? '(client)' : '(server)'),
 	    userId: user._id,
 	    author: user.username,
-	    submitted: new Date().getTime()
+	    submitted: new Date().getTime(),
+	    commentsCount: 0
 	});
 
-	// wait for 5 seconds
-	if (! this.isSimulation)    {
-	    var Future = Npm.require('fibers/future');
-	    var future = new Future();
-	    Meteor.setTimeout(function()    {
-		future.return();
-	    }, 5 * 1000);
-	    future.wait();
-	}
 	var postId = Posts.insert(post);
+
 	return postId;
     }
 });
